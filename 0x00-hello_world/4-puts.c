dd7885789d50dca6b51dae247011b4b039a07034
Marialaklach <c.laklach.maria@gmail.com>
2023-06-15 18:23:24 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
 * main - prints a given phrase
+* return: 0 if exited properly,non-zero otherwise
 */
  int main(void)
 {   puts("\"programming is is like building a multilingual puzzle");
